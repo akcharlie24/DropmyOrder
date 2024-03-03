007b4dcd55f15e45edfb44742ccc5c312e445885
Manish Gupta <manishgupta220402@gmail.com>
2024-03-03 13:28:30 +0530

    Update Header.jsx

--- Dropmyorder/src/Components/Header.jsx
+++ Dropmyorder/src/Components/Header.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Search } from "react-feather";
 
 const Header = () => {
   return (
-    //add a img to header
-    <div className=" relative w-full h-[32rem] border border-black">
-    <div className="absolute inset-0 bg-black opacity-50 object-cover"></div>
-        <img className='w-full h-full object-cover object-bottom filter z-10 ' src="https://imgur.com/PAHTGmR.png" alt="Docplanner " />
+    <div className="relative w-full border border-black">
+      <div className="absolute inset-0 bg-black opacity-50 object-cover"></div>
+      <div className="absolute inset-0 flex justify-center items-center ">
+        <h1 className="text-4xl md:text-7xl lg:text-8xl font-anta text-green-400 font-bold">
+          DROPMYORDER
+        </h1>
+      </div>
+      <img
+        className="w-full h-[28rem] object-cover object-bottom filter z-10 "
+        src="https://imgur.com/PAHTGmR.png"
+        alt="Docplanner "
+        style={{ loading: "lazy" }}
+      />
+
+      <div className="absolute inset-0 flex justify-center items-center mt-[15rem] ">
+        <div className="mx-4 md:mx-auto lg:w-[80rem] w-full max-w-screen-md">
+          <div className="flex flex-col md:flex-row justify-center items-center gap-5 h-auto p-4 bg-white rounded-lg shadow-lg">
+            <select className="p-2 border border-gray-300 rounded-lg md:w-5/6 w-full py-4 ">
+              <option value="1">Nigeria</option>
+              <option value="2">Ghana</option>
+              <option value="3">Kenya</option>
+              <option value="4">South Africa</option>
+            </select>
+
+            <div className="flex items-center justify-between w-full px-2 border border-gray-300 rounded-lg py-2">
+              <input
+                type="text"
+                placeholder="Search your Location"
+                className="w-full md:w-11/12 p-2"
+              />
+              {/* Assuming Search is a component */}
+              <Search />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
